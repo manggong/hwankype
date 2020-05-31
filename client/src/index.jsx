df1ab5446527eb64871234bceee036a341472016
manggong <jihwank1995@gmail.com>
2020-05-31 20:34:43 +0900

    Modify index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./containers/HomePage";
+import Room from "./containers/RoomPage";
+import NotFound from "./components/NotFound";
+import style from "./main.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="room/:room" component={Room} />
+        <Route paht="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("app")
 );
